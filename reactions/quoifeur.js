87e7d41124785f8caedfe54c74b9341532a932e4
Jean Dupouy <jean.dupouy@rednet.io>
2020-08-18 10:17:48 +0200

    add new familiar reaction

---
+++ reactions/quoifeur.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const username = process.env.QUOIFEUR_USERNAME;
+
+const texts = [
+  'FEUR',
+  'FEEEEEEEEUUUUUUUUR',
+  '*FEUR !!!*',
+  ':b_f::b_e::b_u::b_r::space::exclamation:',
+  '```\nF E U R\nE U R F\nU R F E\nR F E U\n```',
+  '𝓕𝓮𝓾𝓻.',
+];
+
+const quoifeur = {
+  regexp: /quoi[^0-9A-Za-z]*$/,
+  async run([], { web, channel }) {
+    await web.chat.postMessage({
+      channel,
+      username,
+      text: getText(),
+      icon_emoji: ':feur:',
+    });
+  },
+};
+
+const getText = () => {
+  return texts[Math.floor(Math.random() * texts.length)];
+};
+
+module.exports = quoifeur;
